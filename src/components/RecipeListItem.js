38c1a2db36528e588179946a9fa37856aa6d428a
Rohit Gupta <94480941+RohitGupta1235@users.noreply.github.com>
2023-05-10 21:22:13 +0530

    Update RecipeListItem.js

--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 const RecipeListItem = ({ recipe }) => {
+  const [clickedRecipe, setClickedRecipe] = useState(null);
+
+  useEffect(() => {
+    if (clickedRecipe) {
+      localStorage.setItem(
+        clickedRecipe.recipe_id,
+        JSON.stringify(clickedRecipe)
+      );
+      console.log(`Added ${clickedRecipe.title} to Favorites!`);
+    }
+  }, [clickedRecipe]);
+
+  const handleIcon = () => {
+    setClickedRecipe(recipe);
+  };
+
   return (
-    <Card>
+    <Card className="recipe-card">
       <img src={recipe.image_url} alt="thumbnail" style={{ height: 170 }} />
       <Card.Content>
         <Card.Header content={recipe.title} />
@@ -25,6 +44,11 @@ const RecipeListItem = ({ recipe }) => {
           content="Recipe URL"
           color="green"
           size="tiny"
+        />
+        <FontAwesomeIcon
+          onClick={handleIcon}
+          icon={faHeart}
+          className={clickedRecipe ? "fav-icons-red" : "fav-icons-pink"}
         />
       </Card.Content>
     </Card>
